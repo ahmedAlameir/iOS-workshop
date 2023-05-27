515cf97dc2d13cbb1131cc2e7cf78afca047a63e
Ammar El-Riyali <ammarelriyali@gmail.com>
2023-05-27 18:17:46 +0300

    added Recipe Collection Cell

--- iOS workshop/Custom View/Cells/RecipeCollectionCell/RecipeCollectionCell.swift
+++ iOS workshop/Custom View/Cells/RecipeCollectionCell/RecipeCollectionCell.swift
@@ -1,47 +1,38 @@
-//
-//  RecipeCell.swift
+
+//  RecipeCollectionCell.swift
 //  iOS workshop
 //
-//  Created by ammar on 26/05/2023.
+//  Created by ammar on 27/05/2023.
 //
 
 import UIKit
-import SDWebImage
 
-class RecipeCell: UITableViewCell {
-    
-    @IBOutlet weak var mealImage: UIImageView!
-    
-    @IBOutlet weak var favBackgroundView: UIView!
-    
+class RecipeCollectionCell: UICollectionViewCell {
     @IBOutlet weak var recipeName: UILabel!
     
     @IBOutlet weak var chefName: UILabel!
-    
-    @IBOutlet weak var foodTypeName: UILabel!
-    
+    @IBOutlet weak var typeName: UILabel!
+    @IBOutlet weak var backgroundFav: UIView!
+    @IBOutlet weak var backgroundreicpe: UIImageView!
+    @IBOutlet weak var favImage: UIImageView!
     @IBOutlet weak var servingsNumber: UILabel!
     
-    @IBOutlet weak var favImage: UIImageView!
-    
-    
     var onClick : (()-> Void)!
-    
     override func awakeFromNib() {
         super.awakeFromNib()
+        // Initialization code
+        backgroundFav.clipsToBounds = true
         
-        favBackgroundView.clipsToBounds = true
+        backgroundFav.layer.cornerRadius = backgroundFav.frame.width / 4
         
-        favBackgroundView.layer.cornerRadius = favBackgroundView.frame.width / 4
+        backgroundreicpe.clipsToBounds = true
         
-        mealImage.clipsToBounds = true
-        
-        mealImage.layer.cornerRadius = mealImage.frame.height / 10
+        backgroundreicpe.layer.cornerRadius = backgroundreicpe.frame.height / 10
         
         let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped(tapGestureRecognizer:)))
         favImage.isUserInteractionEnabled = true
         favImage.addGestureRecognizer(tapGestureRecognizer)
-        
+ 
     }
     
     @objc func imageTapped(tapGestureRecognizer: UITapGestureRecognizer)
@@ -49,13 +40,6 @@ class RecipeCell: UITableViewCell {
         let tappedImage = tapGestureRecognizer.view as! UIImageView
         onClick()
     
-    }
-    
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-        
-        
-        
     }
     func changeImage(_ isFav : Bool){
         if(isFav){
@@ -65,15 +49,13 @@ class RecipeCell: UITableViewCell {
             favImage.image = UIImage(systemName: "heart")
         }
     }
-    
-    
     func initializeCell(meal : Meal,Favourite isFav:Bool,_ onClick : @escaping ()-> Void){
         self.onClick = onClick
         recipeName.text = meal.name
         chefName.text = meal.credits?[0].name ?? "None"
-        foodTypeName.text = meal.credits?[0].type ?? "None"
+        typeName.text = meal.credits?[0].type ?? "None"
         servingsNumber.text = String(meal.numServings ?? 0)
-        mealImage.sd_setImage(with: URL(string: meal.thumbnailURL ?? ""), placeholderImage : UIImage(named: "ic_RecipeCell"))
+        backgroundreicpe.sd_setImage(with: URL(string: meal.thumbnailURL ?? ""), placeholderImage : UIImage(named: "ic_RecipeCell"))
         if(isFav){
             favImage.image = UIImage(systemName: "heart.fill")
         }
@@ -82,5 +64,5 @@ class RecipeCell: UITableViewCell {
             
         }
     }
-    
+
 }
